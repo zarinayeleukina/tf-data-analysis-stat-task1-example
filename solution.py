5523eb14f15f9f8d22d64e7eb29ee00adbb7d894
Zarina Yeleukina <95769264+zarinayeleukina@users.noreply.github.com>
2023-04-07 21:07:35 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
+import math
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 616199784 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    t = np.full(x.shape, 4)
+    lt = x.sum()
+    l = lt / t.mean()
+    return д
